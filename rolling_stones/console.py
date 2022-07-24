4967dc82efa30a32a20548d4fa7962d88632ea99
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-07-24 11:33:21 -0400

    add files

---
+++ rolling_stones/console.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+songs_url = "./data/top-500-songs.txt"
+songs_df = pd.read_csv(songs_url, sep='\t', header = None, names = ['rank', 'song', 'artist', 'year'])
+songs = songs_df.to_dict('records')
+
+track_url = "./data/track_data.json"
+albums_and_tracks = pd.read_json(track_url)
+albums_tracks = albums_and_tracks.to_dict('records')
+
+albums_url = "./data/data.csv"
+df = pd.read_csv(albums_url)
+albums = df.to_dict('records')
+print(albums[0])
